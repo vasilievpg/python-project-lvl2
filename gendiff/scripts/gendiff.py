1d55fc5f82ca5891362814bdf4784e8d0786d532
Pavel Vasilev <vasilievpg@gmail.com>
2022-02-16 11:10:47 +0300

    Добавил в вывод справки -f FORMAT, --format FORMAT

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,6 +5,7 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('-f', '--format', help='set format of output')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     args = parser.parse_args()
