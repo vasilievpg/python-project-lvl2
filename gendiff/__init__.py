4ea4764677cf6593b0613e2d332968f132616b17
Pavel Vasilev <vasilievpg@gmail.com>
2022-02-16 15:57:04 +0300

    Починил импорт

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,4 +1,4 @@
-from generate_diff import generate_diff
+from gendiff.generate_diff import generate_diff
 
 
 __all__ = (generate_diff,)
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,4 +1,4 @@
-from gendiff import generate_diff
+from gendiff.generate_diff import generate_diff
 
 
 def test_generate_diff():
