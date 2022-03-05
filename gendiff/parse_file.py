29fa87eaaa5add6aeeebe20f1234119ca5ca7155
Pavel Vasilev <vasilievpg@gmail.com>
2022-03-05 14:52:18 +0300

    Переделал получение расширения на os.path.splitext(file_path)

--- gendiff/parse_file.py
+++ gendiff/parse_file.py
@@ -1,12 +1,13 @@
 import json
+import os
 import yaml
 
 
 def open_file(file_path):
     with open(file_path) as file:
-        file_extension = file_path[-4:]
-        if file_extension == 'json':
+        _, file_extension = os.path.splitext(file_path)
+        if file_extension == '.json':
             data = json.load(file)
-        if file_extension == 'yaml' or file_extension == '.yml':
+        if file_extension == '.yaml' or file_extension == '.yml':
             data = yaml.load(file, Loader=yaml.Loader)
         return data
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -9,11 +9,11 @@ def open_txt_file(file_path):
 
 def test_generate_diff_json():
     result = generate_diff('./tests/fixtures/file1.json', './tests/fixtures/file2.json')
-    expected_value = open_txt_file('./tests/fixtures/expected_value.txt')
+    expected_value = open_txt_file('./tests/fixtures/diff_plain.txt')
     assert result == expected_value
 
 
 def test_generate_diff_yaml():
     result = generate_diff('./tests/fixtures/file1.yaml', './tests/fixtures/file2.yml')
-    expected_value = open_txt_file('./tests/fixtures/expected_value.txt')
+    expected_value = open_txt_file('./tests/fixtures/diff_plain.txt')
     assert result == expected_value
